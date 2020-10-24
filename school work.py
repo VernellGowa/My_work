eb07eb616efd9f8c3630f413d49737c98dfcf212
Mpagi <mpagik@hotmail.com>
2020-10-24 16:47:43 +0100

    changed to Riya

--- school work.py
+++ school work.py
@@ -2,7 +2,7 @@ import random
 
 
 answer1 = ("Absolutely!!!!")
-answer2 = ("No way Mpagi!") 
+answer2 = ("No way Riya!") 
 answer3 = ("Go on then, Mate!")
 print("Welcome to the Magic 8 Ball-game, use it to answer your questions.")
 question = input("Ask me for any advice and i'll help you out. Type in your answer and then press Enter for an answer.")
